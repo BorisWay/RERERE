8edafe45c1474f57f634ec300976e56eff4b2f08
BorisWay <vatniqueiii@gmail.com>
2022-04-04 23:19:53 +0300

    CALC

--- JS/JS5/index.js
+++ JS/JS5/index.js
@@ -1,53 +1,52 @@
-var buttonPlus = document.getElementById('buttonPlus');
-var buttonMinus = document.getElementById('buttonMinus');
-var buttonMultiply = document.getElementById('buttonMultiply');
-var buttonDevide = document.getElementById('buttonDevide');
+// var buttonPlus = document.getElementById('buttonPlus');
+// var buttonMinus = document.getElementById('buttonMinus');
+// var buttonMultiply = document.getElementById('buttonMultiply');
+// var buttonDevide = document.getElementById('buttonDevide');
 
-function onButtonPlusClick(){
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
+var operationButtons = document.getElementsByClassName('operation-button');
 
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
 
-    var summ = number1+number2;
-    window.alert(summ);
+function getNumber(variable){
+    return Number(variable.value);
 }
 
-function onButtonMinusClick(){
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1-number2;
-    window.alert(summ);
+function makeOperation(operationCode){
+    var result;
+    var number1=getNumber(input1);
+    var number2=getNumber(input2);
+    switch(operationCode){
+        case '+':
+            result=number1+number2;
+            break;
+        case '-':
+            result=number1-number2;
+            break;
+        case '*':
+            result=number1*number2;
+            break;
+        case '/':
+            result=number1/number2;
+            break;
+        default:
+            result='Сценарий не инициализирован'    
+    }
+    window.alert(result);
 }
 
-function onButtonMultiply(){
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1*number2;
-    window.alert(summ);
+function onOperationButtonClick(eventObject)
+{
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
 }
 
-function onButtonDevide(){
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
+function addCommonEventListener(i){
+    operationButtons[i].addEventListener('click'. onOperationButtonClick); 
 
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1/number2;
-    window.alert(summ);
 }
 
-buttonPlus.addEventListener('click', onButtonPlusClick);
-buttonMinus.addEventListener('click', onButtonMinusClick);
-buttonMultiply.addEventListener('click', onButtonMultiply);
-buttonDevide.addEventListener('click', onButtonDevide);
+for (let i = 0; i < operationButtons.length; i++) {
+    operationButtons[i].addEventListener('click', onOperationButtonClick);
+}
